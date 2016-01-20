b6eb7914f56494c3cc208da7659002040d6762f3
Kazutaka Sankai <Sankai_Kazutaka@ogis-ri.co.jp>
2016-01-20 17:00:44 +0900

    closed #3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     #strong parametersを設定し、usernameを許可
     devise_parameter_sanitizer.for(:sign_up, ) { |u| u.permit(:email, :name, :password, :password_confirmation, :current_password) }
-    devise_parameter_sanitizer.for(:account_update, ) { |u| u.permit(:email, :name, :password, :password_confirmation, :current_password) }
+    devise_parameter_sanitizer.for(:account_update, ) { |u| u.permit(:email, :name, :password, :password_confirmation, :current_password, :isadmin) }
   end  
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150625075411) do
+ActiveRecord::Schema.define(version: 20160120074752) do
 
   create_table "books", force: true do |t|
     t.string   "name"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20150625075411) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.boolean  "isadmin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
